f91192494c1db46e8efa5e628584ff6403bd7b28
masutaniR <popohu87@gmail.com>
2021-02-14 09:01:39 +0000

    Add ジャンルindexページ

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,16 @@
 class Admin::GenresController < ApplicationController
+    def index
+      @genres = Genre.all
+      @genre = Genre.new
+    end
+
+    def edit
+      @genre = Genre.find(params[:id])
+    end
+
+    def create
+    end
+
+    def update
+    end
 end
