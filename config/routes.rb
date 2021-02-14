a9496856cbe1811ab9bc09545db36e51846c9239
masutaniR <popohu87@gmail.com>
2021-02-14 07:58:28 +0000

    Add 管理者ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admin
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
