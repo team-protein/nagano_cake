346f250b676fdf418a234d32ed55ba382f9bb7b4
masutaniR <popohu87@gmail.com>
2021-02-15 07:36:47 +0000

    Add productコントローラ・showページ

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,10 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    @tax_included_price = @product.price * 1.1
+  end
 end
