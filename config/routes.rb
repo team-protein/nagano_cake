6897100c521a07705ee75203ab158cac9e75253d
masutaniR <75651339+masutaniR@users.noreply.github.com>
2021-02-15 13:15:44 +0900

    Revert "Customers"

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,25 @@
 Rails.application.routes.draw do
   devise_for :admins
-  devise_for :customers, controllers: {
-    registrations: 'customers/registrations'
-  }
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homes#top"
   get "about" => "homes#about"
-
-  resource :customers, only: [:edit, :update]
-  get "customers/my_page" => "customers#show"
+  
+  resource :customers, only: [:show, :edit, :update]
   get "customers/delete_confirm" => "customers#delete_confirm"
   patch "customers/withdraw" => "customers#withdraw"
-
+  
   resources :products, only: [:index, :show]
-
+  
   resources :cart_products, only: [:index, :update, :destroy, :create]
   delete "cart_products/destroy_all" => "cart_products#destroy_all"
-
+  
   resources :orders, only: [:new, :create, :index, :show]
   post "orders/confirm" => "orders#confirm"
   get "orders/complete" => "orders#complete"
-
+  
   resources :addresses, except: [:new, :show]
-
+  
   namespace :admin do
     resources :customers, only: [:index, :show, :edit, :update]
     resources :products, except: [:destroy]
@@ -31,5 +28,5 @@ Rails.application.routes.draw do
       resources :ordered_products, only: [:update]
     end
   end
-
+  
 end
