b412c2fe7fa0395d0e8b8f7ee58a2ed4cc63b2ce
akf38 <dpv28779@gmail.com>
2021-02-15 01:53:48 +0000

    Add after_sign_inの条件分岐を調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
     customers_path
   end
   
+  def after_sign_in_path_for(resource)
+      case resource
+      when Admin
+        admin_orders_path
+      when Customer
+        root_url
+      end
+  end
+  
   def after_sign_out_path_for(resource)
     root_url
   end
