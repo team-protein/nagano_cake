046dd9c60224898eb478b5885172dbcec39b5555
masutaniR <popohu87@gmail.com>
2021-02-15 00:56:49 +0000

    Add 管理者 productモデル・コントローラ

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,3 +1,43 @@
 class Admin::ProductsController < ApplicationController
   before_action :authenticate_admin!
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_product_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @products = Product.page(params[:page]).reverse_order
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update
+      redirect_to admin_product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  private
+    def product_params
+      params.require(:product).permit(:genre_id, :name, :description, :price, :is_active, :image_id)
+    end
+
 end
