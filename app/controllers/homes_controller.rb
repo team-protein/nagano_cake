a211801302675ad9afb4af2295ccd3bfe5dca8b4
masutaniR <popohu87@gmail.com>
2021-02-15 23:06:26 +0000

    Add TOPページ新着商品

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
 
   def top
-    # @products = Product.where(is_active: true).order("created_at DESC").limit(4)
+    @products = Product.where(is_active: true).order("created_at DESC").limit(4)
   end
 
   def about
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,7 @@ module ApplicationHelper
 
   # 税込価格に三桁区切りのカンマを入れる
   def tax_included_price(price)
-    price = price*1.1
+    price = price*1.08
     price = price.floor
     "#{price.to_s(:delimited, delimiter: ',')}円"
   end
