d06300b4616138a708aab8bb4ff7ffc4d07c1821
fukiwake <tak0203753@gmail.com>
2021-02-16 05:44:42 +0000

    finish newのビュー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,22 @@
 class OrdersController < ApplicationController
+  
+  def new
+    @addresses = current_customer.addresses.to_a.map { |address| [address.postcode + address.address + address.dear, address.id] }
+    
+  end
+  
+  def confirm
+  end
+  
+  def create
+  end
+  
+  def complete
+  end
+  
+  def index
+  end
+  
+  def show
+  end
 end
