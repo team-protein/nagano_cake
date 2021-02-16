870bb222ead82b9007098c98f99fb86f751fa6bb
fukiwake <tak0203753@gmail.com>
2021-02-16 06:00:59 +0000

    finish createコントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,29 @@ class OrdersController < ApplicationController
   
   def new
     @addresses = current_customer.addresses.to_a.map { |address| [address.postcode + address.address + address.dear, address.id] }
-    
   end
   
   def confirm
   end
   
   def create
+    order = current_customer.orders.new(payment_method: params[:payment_method])
+    if params[:shipping_method] == 0
+      order.postcode = current_customer.postcode
+      order.address = current_customer.address
+      order.dear = current_customer.last_name + current_customer.first_name
+    elsif params[:shipping_method] == 1
+      address = Address.find(params[:address_id])
+      order.postcode = address.postcode
+      order.address = address.address
+      order.dear = address.dear
+    else 
+      order.postcode = params[:postcode]
+      order.address = params[:address]
+      order.dear = params[:dear]
+    end
+    order.save
+    redirect_to orders_confirm_path(order)
   end
   
   def complete
