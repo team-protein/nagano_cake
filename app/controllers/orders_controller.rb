a7a1f8853f2f81443799483de5077cbcf9a60273
fukiwake <tak0203753@gmail.com>
2021-02-16 11:01:13 +0000

    update orderコントローラー

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,32 +1,56 @@
 class OrdersController < ApplicationController
-  
+
   def new
+    unless current_customer.cart_products.present?
+      flash[:alert] = "商品をカートに入れてください"
+      render "cart_products/index"
+    end
     @addresses = current_customer.addresses.to_a.map { |address| [address.postcode +  address.address +  address.dear, address.id] }
   end
-  
+
   def confirm
     @order = current_customer.orders.new(payment_method: params[:payment_method])
-    if params[:shipping_method] == "0"
+    if params[:shipping_to] == "0"
       @order.postcode = current_customer.postcode
       @order.address = current_customer.address
       @order.dear = current_customer.last_name + current_customer.first_name
-    elsif params[:shipping_method] == "1"
+    elsif params[:shipping_to] == "1"
       address = Address.find(params[:address_id])
       @order.postcode = address.postcode
       @order.address = address.address
       @order.dear = address.dear
-    else 
+    else
       @order.postcode = params[:postcode]
       @order.address = params[:address]
       @order.dear = params[:dear]
     end
+    session[:payment_method] = params[:payment_method]
+    session[:shipping_to] = params[:shipping_to]
+    session[:address_id] = params[:address_id]
+    session[:postcode] = params[:postcode]
+    session[:address] = params[:address]
+    session[:dear] = params[:dear]
+    render :new if @order.invalid?
+  end
+
+  def create
+    @order = current_customer.orders.new(payment_method: session[:payment_method])
+    if session[:shipping_to] == "0"
+      @order.postcode = current_customer.postcode
+      @order.address = current_customer.address
+      @order.dear = current_customer.last_name + current_customer.first_name
+    elsif session[:shipping_to] == "1"
+      address = Address.find(session[:address_id])
+      @order.postcode = address.postcode
+      @order.address = address.address
+      @order.dear = address.dear
+    else
+      @order.postcode = session[:postcode]
+      @order.address = session[:address]
+      @order.dear = session[:dear]
+    end
     @order.shipping_cost = 800
     @order.total_price = current_customer.cart_products.sum{|cart_product|cart_product.product.price * 1.1 * cart_product.quantity} + @order.shipping_cost
-    return if @order.valid?
-    render :new
-  end
-  
-  def create
     if @order.save
       current_customer.cart_products.each do |cart_product|
         ordered_product = @order.ordered_products.new
@@ -40,15 +64,16 @@ class OrdersController < ApplicationController
     else
       render :new
     end
-    
+
   end
-  
+
   def complete
   end
-  
+
   def index
   end
-  
+
   def show
   end
+
 end
