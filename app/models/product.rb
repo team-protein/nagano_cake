5b5a9b702a73558b4a2cf7eceac63b5dd811f9af
masutaniR <popohu87@gmail.com>
2021-02-16 08:52:33 +0000

    Add admin検索機能

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,17 @@ class Product < ApplicationRecord
   belongs_to :genre
   has_many :cart_products, dependent: :destroy
   attachment :image
+
+  # 検索用メソッド
+	def self.search_for(content, method)
+	  if method == 'perfect'
+	  	Product.where('name LIKE?', "#{content}")
+	  elsif method == 'forward'
+	  	Product.where('name LIKE?', "#{content}%")
+	  elsif method == 'backward'
+	  	Product.where('name LIKE?', "%#{content}")
+	  else
+	  	Product.where('name LIKE?', "%#{content}%")
+	  end
+	end
 end
