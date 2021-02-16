9ec5301bef640edfd0249918d3f17ada2aad1312
fukiwake <tak0203753@gmail.com>
2021-02-16 00:41:18 +0000

    test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create!(email: ENV['ADMIN_EMAIL'],
-             password: ENV['ADMIN_PASSWORD'])
+Admin.create!(email: ENV['ADMIN_EMAIL'], password: ENV['ADMIN_PASSWORD'])
