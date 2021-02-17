d96b2e59790a5bb96c312e999765fa69615b52da
masutaniR <popohu87@gmail.com>
2021-02-17 07:56:46 +0000

    Update flashメッセージ

--- app/controllers/cart_products_controller.rb
+++ app/controllers/cart_products_controller.rb
@@ -5,13 +5,19 @@ class CartProductsController < ApplicationController
     @cart_product = CartProduct.new(cart_product_params)
     @cart_product.customer_id = current_customer.id
     if current_customer.cart_products.find_by(product_id: params[:cart_product][:product_id]).present?
-      @cart_product = current_customer.cart_products.find_by(product_id: params[:cart_product][:product_id])
-      @cart_product.quantity += params[:cart_product][:quantity].to_i
-      @cart_product.save
-      flash[:notice] = "商品をカートに追加しました。"
-      redirect_to cart_products_path
+      if params[:cart_product][:quantity] == ""
+        @product = @cart_product.product
+        flash.now[:alert] = '個数を入力してください'
+        render 'products/show'
+      else
+        @cart_product = current_customer.cart_products.find_by(product_id: params[:cart_product][:product_id])
+        @cart_product.quantity += params[:cart_product][:quantity].to_i
+        @cart_product.save
+        flash[:notice] = "商品をカートに追加しました。"
+        redirect_to cart_products_path
+      end
     elsif @cart_product.save
-      flash[:notice] = "商品をカートに追加しました。"
+      flash[:notice] = "新たな商品をカートに追加しました。"
       redirect_to cart_products_path
     else
       @product = @cart_product.product
