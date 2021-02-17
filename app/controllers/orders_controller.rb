edf71303ab8f168feef0b39ce99db7047b3be574
fukiwake <tak0203753@gmail.com>
2021-02-17 01:38:14 +0000

    finish order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,28 +7,35 @@ class OrdersController < ApplicationController
       @total_price = @cart_products.sum{|cart_product|cart_product.product.price * 1.1 * cart_product.quantity}
       render "cart_products/index"
     end
-    @addresses = current_customer.addresses.to_a.map {|address| [address.postcode +  address.address +  address.dear, address.id]}
+    @addresses = current_customer.addresses.to_a.map {|address| ["〒#{address.postcode} #{address.address} #{address.dear}", address.id]}
   end
 
   def confirm
     @order = current_customer.orders.new(payment_method: params[:payment_method])
+    # お届け先：ご自身の住所
     if params[:shipping_to] == "0"
       @order.postcode = current_customer.postcode
       @order.address = current_customer.address
       @order.dear = current_customer.last_name + current_customer.first_name
+    # お届け先：登録済み住所から選択
     elsif params[:shipping_to] == "1"
       address = Address.find(params[:address_id])
       @order.postcode = address.postcode
       @order.address = address.address
       @order.dear = address.dear
     else
+    # お届け先：新しいお届け先
       @order.postcode = params[:postcode]
       @order.address = params[:address]
       @order.dear = params[:dear]
     end
     @order.shipping_cost = 800
     @order.total_price = current_customer.cart_products.sum{|cart_product|cart_product.product.price * 1.1 * cart_product.quantity} + @order.shipping_cost
+    @ordered_products_sum = @order.total_price - @order.shipping_cost
     session[:order] = @order
+    session[:postcode] = params[:postcode]
+    session[:address] = params[:address]
+    session[:dear] = params[:dear]
     render :new if @order.invalid?
   end
 
@@ -43,6 +50,10 @@ class OrdersController < ApplicationController
         ordered_product.tax_included_price = cart_product.product.price * 1.1 * cart_product.quantity
         ordered_product.save
       end
+      current_customer.addresses.create(postcode: session[:postcode], address: session[:address], dear: session[:dear])
+      session[:postcode] = nil
+      session[:address] = nil
+      session[:dear] = nil
       current_customer.cart_products.destroy_all
       redirect_to complete_orders_path
     else
@@ -59,6 +70,7 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
+    @ordered_products_sum = @order.total_price - @order.shipping_cost
   end
 
 end
