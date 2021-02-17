9ccda9ba05bfd547ecb3c7e54f2c742872b4730b
masutaniR <popohu87@gmail.com>
2021-02-17 02:27:49 +0000

    Add 販売中商品のみ検索

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def search
+    @content = params[:content]
+    @genre = params[:genre]
+    @records = Product.genre_search_for(@content, @genre).order("created_at DESC").page(params[:page]).per(8)
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -6,8 +6,18 @@ class Product < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   attachment :image
 
-  # 検索用メソッド
+  # 管理者側検索用メソッド
 	def self.search_for(content)
-	  	Product.where('name LIKE?', "%#{content}%")
+	  Product.where('name LIKE?', "%#{content}%")
 	end
+
+  # 会員側検索用メソッド
+  def self.genre_search_for(content, genre)
+	  Product.where(is_active: true).where("name LIKE? AND genre_id LIKE?", "%#{content}%", "#{genre}")
+	  # genre_ids = Product.where("genre_id = ?").pluck(:id)
+    # @submit_searched = Product.where("submit_id IN (?) or submit_id IN (?)", submit_name_ids, problem_name_ids)
+    # #submit_name_idsかproblem_name_idsに当てはまるものをsubmitテーブルから探して、
+    # #@submit_searchedに代入。
+    # Classmate.where(class_id: 2).where(club_name: 'tennis')
+  end
 end
