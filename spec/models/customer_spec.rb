841e4dc6669d1e1fa145c80da5fe032337e97c55
akf38 <dpv28779@gmail.com>
2021-02-18 08:56:19 +0000

    Add FactoryBot customer address

---
+++ spec/models/customer_spec.rb
@@ -0,0 +1,11 @@
+describe '登録情報変更退会' 
+  
+    it 'マイページにて会員情報編集ボタンを押下する' do
+      click_button
+    end
+  context '足し算' do
+    it '1 + 1 は 2 になる' do
+      expect(1 + 1).to eq 3
+    end
+  end
+end
